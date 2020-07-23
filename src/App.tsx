51e46ba287920c3247ed5f27ca9b83dc7feac031
Lucas Bueno <lucas-jbb@hotmail.com>
2020-07-23 19:29:26 -0300

    Create register page and styles updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import SignIn from './pages/SignIn/index';
+import SignUp from './pages/SignUp/index';
 
 import GlobalStyle from './styles/global';
 
@@ -8,7 +9,7 @@ const App: React.FC = () => {
   return (
     <>
       <GlobalStyle />
-      <SignIn />
+      <SignUp />
     </>
   );
 };
