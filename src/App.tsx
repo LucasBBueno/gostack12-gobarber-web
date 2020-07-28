80b77e940d8e4b1291338296eed0664726b39bbc
Lucas Bueno <lucas-jbb@hotmail.com>
2020-07-28 00:04:31 -0300

    Create AuthContext for use with ContextAPI

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 
 import SignIn from './pages/SignIn/index';
-import SignUp from './pages/SignUp/index';
+// import SignUp from './pages/SignUp/index';
 
 import GlobalStyle from './styles/global';
+
+import AuthContext from './context/AuthContext';
 
 const App: React.FC = () => {
   return (
     <>
+      <AuthContext.Provider value={{ name: 'Lucas' }}>
+        <SignIn />
+      </AuthContext.Provider>
       <GlobalStyle />
-      <SignIn />
     </>
   );
 };
