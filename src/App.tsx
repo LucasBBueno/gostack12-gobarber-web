33f219cae0d54f003d7c4e2ab2ac2e393f3dadfc
Lucas Bueno <lucas-jbb@hotmail.com>
2020-07-30 17:55:11 -0300

    Create toast message with ContextAPI

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,15 @@ import SignIn from './pages/SignIn/index';
 
 import GlobalStyle from './styles/global';
 
-import { AuthProvider } from './hooks/AuthContext';
+import AppProvider from './hooks/index';
 
 const App: React.FC = () => {
   return (
     <>
-      <AuthProvider>
+      <AppProvider>
         <SignIn />
-      </AuthProvider>
+      </AppProvider>
+
       <GlobalStyle />
     </>
   );
